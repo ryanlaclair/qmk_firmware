92f4b56181cf6f555ad63922cd85ec92169e7b68
Ryan LaClair <ryanlaclair@gmail.com>
2018-12-14 21:00:57 -0500

    switched layer 1 and 2

--- keyboards/jd45/keymaps/ryan/keymap.c
+++ keyboards/jd45/keymaps/ryan/keymap.c
@@ -13,13 +13,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, MO(_FN2), MO(_FN1), KC_SPC, KC_RGUI, KC_RALT, MO(_FN1), KC_RCTL),
     [_FN1] = LAYOUT(
-        KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_BSLS,
-        _______, _______, _______, _______, _______, KC_SPC, _______, _______, _______, _______),
-    [_FN2] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSPC,
         _______, _______, _______, _______, _______, _______, KC_END, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+    [_FN2] = LAYOUT(
+        KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,
+        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_BSLS,
+        _______, _______, _______, _______, _______, KC_SPC, _______, _______, _______, _______),
 };
