91a6e6c747e60a58b5d02fc4570e6964e443c940
Ryan LaClair <ryanlaclair@gmail.com>
2018-12-15 12:45:45 -0500

    updated layouts

--- keyboards/jd45/keymaps/ryan/keymap.c
+++ keyboards/jd45/keymaps/ryan/keymap.c
@@ -3,23 +3,29 @@
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _FN1 1
-#define _FN2 2
+#define _NAV 1
+#define _NUM 2
+#define _PAREN 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT, KC_BSPC,
+        LT(_NAV, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_FN2), MO(_FN1), KC_SPC, KC_RGUI, KC_RALT, MO(_FN1), KC_RCTL),
-    [_FN1] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BSPC,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(_PAREN), MO(_NUM), KC_SPC, KC_RGUI, KC_RALT, KC_BSLS, KC_RCTL),
+    [_NAV] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_UP, KC_PGDN, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_END, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
-    [_FN2] = LAYOUT(
-        KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_BSPC,
-        KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT, KC_BSLS,
-        _______, _______, _______, _______, _______, KC_SPC, _______, _______, _______, _______),
+    [_NUM] = LAYOUT(
+        KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______, _______,
+        _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
+        _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+    [_PAREN] = LAYOUT(
+        _______, KC_MINS, KC_EQL, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LPRN, KC_RPRN, _______,
+        _______, KC_UNDS, KC_PLUS, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
 };
